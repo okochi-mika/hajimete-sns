73e9a36ddc589818e5dd39d3cd6bd7c2b22f1554
okochi-mika <hiyoko_m@icloud.com>
2025-08-11 16:36:41 +0900

    文字の装飾

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 <div class="container profile-wrapper my-4">
-  <h2 class="mb-4">プロフィール情報</h2>
+  <h2 class="mb-4 profile-title">プロフィール情報</h2>
 
   <form method="POST" action="{{ route('profile.update') }}">
     @csrf
