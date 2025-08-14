10b9575aa8a0e888d4a2597ded60494924cc53f9
okochi-mika <hiyoko_m@icloud.com>
2025-08-14 16:29:51 +0900

    ヘッダーにロゴの追加

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,27 +1,31 @@
 <header>
    <nav class="navbar navbar-light bg-light">
        <div class="container">
-           <a href="{{ route('posts.index') }}" class="fs-2 navbar-brand m-3 ms-5">ちょるのったー</a>
+            
+            {{-- ロゴ（高さは自由に調整可能） --}}
+            <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
+                <img src="{{ asset('images/logo.sns.png') }}" 
+                     alt="ちょるのったー" 
+                     class="logo-img" 
+                     style="height:70px; width:auto;">
+            </a>
 
-           <ul class="navbar-nav d-flex flex-row align-items-center">
-               <li class="nav-item me-3">
-                   @php
-                       $user = auth()->user();
-                   @endphp
-
-                  @if ($user && ($user->bio || $user->avatar))  
-                  <a href="{{ route('profile.show') }}" class="nav-link">{{ $user->name }}</a>
-                  @else
-                  <a href="{{ route('profile.show') }}" class="nav-link">{{ $user->name }}</a>
-                  @endif
-               </li>
-               <li class="nav-item">
-                   <a href="{{ route('logout') }}" class="nav-link" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">ログアウト</a>
-                   <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                       @csrf
-                   </form>
-               </li>
-           </ul>
-       </div>
-   </nav>
+            {{-- ユーザー情報とナビ --}}
+            <ul class="navbar-nav d-flex flex-row align-items-center ms-auto gap-2">
+                <li class="nav-item">
+                    @php $user = auth()->user(); @endphp
+                    <a href="{{ route('profile.show') }}" class="nav-link py-1">{{ $user->name }}</a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route('logout') }}" class="nav-link py-1"
+                       onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                       ログアウト
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
+                </li>
+            </ul>
+        </div>
+    </nav>
 </header>
